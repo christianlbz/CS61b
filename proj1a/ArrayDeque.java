f5a94f6acaf5e767914f6bee18bab0f96906b5df
christianlbz <852573831@qq.com>
2022-04-08 17:23:24 +0800

    add Two achievement of Deque

---
+++ proj1a/ArrayDeque.java
@@ -0,0 +1,148 @@
+public class ArrayDeque<T> {
+    private int head = 0;
+    private int tail = 0;
+    public T[] itemArray;
+    public int size = 0;
+
+    public ArrayDeque(){
+        this.itemArray = (T[])new Object[8];
+    }
+
+    public boolean isEmpty(){
+        return this.head == this.tail;
+    }
+
+    public void addFirst(T item){
+        if (((tail+1)%itemArray.length) == head){
+            System.out.println("The Deque is full.");
+            this.addSize();
+            this.head = this.itemArray.length-1;
+            this.itemArray[this.head] = item;
+            this.size += 1;
+        } else{
+                if (this.head >= 1) {
+                    this.head -= 1;
+                    itemArray[this.head] = item;
+                } else {
+                    this.head = itemArray.length - 1;
+                    itemArray[this.head] = item;
+
+                }
+            this.size += 1;
+            }
+
+
+    }
+
+    public void addLast(T item){
+        if(((tail+1)% itemArray.length)==head){
+            System.out.println("The Deque is full.");
+            this.addSize();
+            this.itemArray[this.tail] = item;
+            this.tail += 1;
+        } else{
+            if(this.tail<= itemArray.length-1){
+                itemArray[tail] = item;
+                this.tail += 1;
+            } else{
+                itemArray[0] = item;
+                this.tail = 0;
+            }
+            this.size += 1;
+        }
+    }
+
+    public T removeFirst(){
+        if (this.isEmpty()) {
+            System.out.println("The Deque is Empty");
+            return null;
+        } else {
+            T temp = this.itemArray[head];
+            this.itemArray[head] = null;
+            if (this.head < this.itemArray.length-1){
+                this.head += 1;
+            } else {
+                this.head = 0;
+            }
+            this.size -= 1;
+            if (this.size%7==0){
+                this.reduceSize();
+            }
+            return temp;
+        }
+    }
+
+    public T removeLast(){
+        if (this.isEmpty()){
+            System.out.println("The Deque is Empty");
+            return null;
+        } else {
+            T temp;
+            if (this.tail > 0){
+                this.tail -= 1;
+                temp = this.itemArray[this.tail];
+                this.itemArray[this.tail] = null;
+            } else{
+                this.tail = this.itemArray.length-1;
+                temp = this.itemArray[this.tail];
+                this.itemArray[this.tail] = null;
+            }
+            this.size -= 1;
+            if (this.size%7==0 && this.size != 0){
+                this.reduceSize();
+            }
+            return temp;
+        }
+    }
+
+    public int size(){
+        return this.size;
+    }
+
+    public T get(int index){
+        if(this.isEmpty()){
+            System.out.println("The Deque is Empty");
+            return null;
+        } else{
+            if (index > this.size-1){
+                System.out.println("Index of out range");
+                return null;
+            } else{
+                return this.itemArray[(this.head+index)%this.itemArray.length];
+            }
+        }
+    }
+
+    public void printDeque(){
+        String string = "";
+        for (int i = 0;i<this.size;i++){
+            string = string + this.itemArray[(this.head+i)%this.itemArray.length] + " ";
+        }
+        System.out.println(string);
+    }
+
+    public void addSize(){
+        int pair = this.itemArray.length - 1;
+        T[] newArray = (T[]) new Object[(pair+1)*2+1];
+        for(int i = 0;i<this.size;i++){
+            newArray[i] = this.itemArray[(this.head+i)%this.itemArray.length];
+        }
+        this.itemArray = newArray;
+        this.head = 0;
+        this.tail = this.size;
+        System.out.println("Increment the size of Array.");
+    }
+
+    public void reduceSize(){
+        int pair = this.itemArray.length - 1;
+        T[] newArray = (T[]) new Object[(pair-1)*2+1];
+        for (int i = 0;i<this.size;i++){
+            newArray[i] = this.itemArray[(this.head+i)%this.itemArray.length];
+        }
+        this.itemArray = newArray;
+        this.head = 0;
+        this.tail = this.size;
+        System.out.println("Decrease the size of the Array.");
+    }
+
+}
---
+++ proj1a/LinkedListDeque.java
@@ -0,0 +1,120 @@
+public class LinkedListDeque<T> {
+    public class Node<BleepBlorp> {
+        public T item;
+        public Node pre;
+        public Node next;
+
+        public Node(T item, Node pre, Node next){
+            this.item = item;
+            this.pre = pre;
+            this.next = next;
+        }
+    }
+
+    private Node setinel = new Node(null, null, null);
+    private int size = 0;
+    public LinkedListDeque(T val){
+        setinel.next = new Node(val, setinel, setinel);
+        this.size += 1;
+    }
+
+    public LinkedListDeque(){
+
+    }
+
+    public void addFirst(T item){
+        if (setinel.next == null){
+            Node temp = new Node(item, setinel, setinel);
+            setinel.next = temp;
+            setinel.pre = temp;
+        } else {
+            Node temp = setinel.next;
+            setinel.next = new Node(item, setinel, temp);
+            temp.pre = setinel.next;
+        }
+        this.size += 1;
+    }
+
+    public void addLast(T item){
+        if (setinel.pre == null){
+            Node temp = new Node(item, setinel, setinel);
+            setinel.pre = temp;
+            setinel.next = temp;
+        } else{
+            Node temp = setinel.pre;
+            setinel.pre = new Node(item, temp, setinel);
+            temp.next = setinel.pre;
+        }
+        this.size += 1;
+    }
+
+    public boolean isEmpty(){
+        return this.size == 0;
+    }
+
+    public int size(){
+        return this.size;
+    }
+
+    public T removeFirst(){
+        if(setinel.next == null){
+            return null;
+        } else {
+            T val = (T) setinel.next.item;
+            if (setinel.next.next == null){
+                setinel.next = null;
+                setinel.pre = null;
+            } else{
+                setinel.next = setinel.next.next;
+                setinel.next.next.pre = setinel;
+            }
+            this.size -= 1;
+            return val;
+        }
+    }
+
+    public T removeLast(){
+        if (setinel.pre == null){
+            return null;
+        } else {
+            T val = (T) setinel.pre.item;
+            if (setinel.pre.pre == null){
+                setinel.next = setinel;
+                setinel.pre = setinel;
+            } else{
+                setinel.pre = setinel.pre.pre;
+                setinel.pre.next = setinel;
+            }
+            this.size -= 1;
+            return val;
+        }
+    }
+
+    public T get(int index){
+        if (index > this.size-1){
+            System.out.println("index out of range");
+            return null;
+        } else{
+            Node temp = setinel.next;
+            while(index != 0){
+                temp = temp.next;
+                index -= 1;
+            }
+            return (T) temp.item;
+        }
+    }
+
+
+    public void printDeque(){
+        String string = "";
+        Node temp = setinel.next;
+        while(temp != setinel){
+            string = string + temp.item + " ";
+            temp = temp.next;
+        }
+        System.out.println(string);
+    }
+
+
+
+}
